9e10e2588c48984f062dcd931ad55d41e156c451
office <bla@bla.com>
2022-03-24 18:11:05 +0100

    Initial commit

---
+++ app/src/main/java/com/algebra/androidroom/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.algebra.androidroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity( ) {
+    override fun onCreate( savedInstanceState: Bundle? ) {
+        super.onCreate( savedInstanceState )
+        setContentView( R.layout.activity_main )
+    }
+
+    override fun onCreateOptionsMenu( menu: Menu? ): Boolean {
+        menuInflater.inflate( R.menu.menu, menu )
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected( item : MenuItem ) : Boolean {
+        if( item.itemId==R.id.deleteAll ) {
+            Toast.makeText( this, "Brisanje svega...", Toast.LENGTH_SHORT ).show( )
+        }
+        return true
+    }
+}
